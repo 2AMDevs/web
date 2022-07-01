a3a74ba7556f36bfe70ea679fc8f40431f2ba060
Aashutosh Rathi <aashutoshrathi@gmail.com>
2022-07-02 01:58:15 +0530

    init: 🦕

---
+++ routes/index.tsx
@@ -0,0 +1,23 @@
+/** @jsx h */
+import { h } from "preact";
+import { tw } from "@twind";
+
+export default function Home() {
+  return (
+    <div class={tw`p-4 mx-auto max-w-screen-md`}>
+      <img
+        src="/favicon.png"
+        height="100px"
+        width="100px"
+        alt="2AM Devs"
+      />
+      <p class={tw`my-6`}>
+        <pre>
+          Hey! Thanks for coming.
+          <br/>
+          This site is `WIP` 🚧
+        </pre>
+      </p>
+    </div>
+  );
+}
